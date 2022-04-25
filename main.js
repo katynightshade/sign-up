8840451f607ade838bff916d033718a5a1e8de73
katynightshade <98486819+katynightshade@users.noreply.github.com>
2022-04-25 16:36:08 -0600

    attempts at field validation

--- main.js
+++ main.js
@@ -1,24 +1,11 @@
 function checkPass() {
     const password = document.getElementById('user-password');
     const confirmPass = document.getElementById('confirm-pass');
-    password.addEventListener('focusout', () => {
-        if (password.value == confirmPass.value) {
-            password.style.border = '1px solid green';
-            confirmPass.style.border = '1px solid green';
-        } else {
-            password.style.border = '1px solid red';
-            confirmPass.style.border = '1px solid red';
-        }
-    });
-    confirmPass.addEventListener('focusout', () => {
-        if (password.value == confirmPass.value) {
-            password.style.border = '1px solid green';
-            confirmPass.style.border = '1px solid green';
-        } else {
-            password.style.border = '1px solid red';
-            confirmPass.style.border = '1px solid red';
-        }
-    });
+    if (password.value != confirmPass.value) {
+        confirmPass.setCustomValidity('Passwords do not match');
+    } else {
+        confirmPass.setCustomValidity('');
+    }
 }
 
 checkPass();
